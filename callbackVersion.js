0b8e1d0b45d96ec16133b1b8525aca69f57b8229
CheskaM <cheska_isabell0401@yahoo.com>
2025-06-29 22:37:49 -0400

    Trying to track index.html

---
+++ callbackVersion.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+    console.log("1. Mowing the yard.")
+}, 2000);
+
+setTimeout(() => {
+    console.log("2. Weedeating the edges of the house and fence line.")
+}, 1500);
+
+setTimeout(() => {
+    console.log("3. Trimming the hedges.")    
+}, 1000);
+
+setTimeout(() => {
+    console.log("4. Collect fallen wood for summer night fires.")
+}, 2500);
+
+setTimeout(() => {
+    console.log("5. Water garden.")
+}, 500);
+    
+
+
